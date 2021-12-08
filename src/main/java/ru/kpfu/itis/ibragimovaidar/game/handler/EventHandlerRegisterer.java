c972ecf4080c0dac73ff81c029735379d627cc1c
ibragimovaidar <ibragimovaidarp@gmail.com>
2021-12-09 01:07:06 +0300

    Add Game event handler

---
+++ src/main/java/ru/kpfu/itis/ibragimovaidar/game/handler/EventHandlerRegisterer.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.ibragimovaidar.game.handler;
+
+import javafx.scene.input.MouseEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ru.kpfu.itis.ibragimovaidar.game.context.GameInstanceContextHolder;
+import ru.kpfu.itis.ibragimovaidar.game.render.GameEntityRenderer;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class EventHandlerRegisterer {
+
+	private final GameInstanceContextHolder context;
+	private final GameEntityRenderer gameEntityRenderer;
+
+	public void register(){
+		// adding tower by click handler
+		context.getRootPane().addEventHandler(MouseEvent.MOUSE_CLICKED, new TowerAddingMouseEventHandler(context, gameEntityRenderer));
+	}
+}
