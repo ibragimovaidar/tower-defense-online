92d8a1ff875183b607f4ba6e5d485534b216409e
ibragimovaidar <ibragimovaidarp@gmail.com>
2021-12-09 00:07:43 +0300

    refactor GameEntity

--- src/main/java/ru/kpfu/itis/ibragimovaidar/entities/GameEntity.java
+++ src/main/java/ru/kpfu/itis/ibragimovaidar/entities/GameEntity.java
@@ -2,39 +2,64 @@ package ru.kpfu.itis.ibragimovaidar.entities;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import ru.kpfu.itis.ibragimovaidar.game.GameContextHolder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import ru.kpfu.itis.ibragimovaidar.game.context.GameInstanceContextHolder;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+@Slf4j
+@Getter
+@Setter
 public abstract class GameEntity {
 
-	protected int x;
-	protected int y;
+	protected double x;
+	protected double y;
 	protected ImageView imageView;
 	protected int width;
 	protected int height;
 
+	protected GameEntity(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	protected GameEntity(int x, int y, int width, int height, Image image){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.imageView = new ImageView(image);
+	}
 
 	public GameEntity(int x, int y, Path imagePath, int width, int height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		this.imageView = loadImage(imagePath);
+		this.imageView = loadImageView(imagePath);
 	}
 
-	protected ImageView loadImage(Path imagePath){
-		try (InputStream imageInputStream = Files.newInputStream(imagePath)) {
-			return new ImageView(new Image(imageInputStream, width, height, false, false));
+	protected ImageView loadImageView(Path imagePath){
+		return new ImageView(loadImage(imagePath, width, height));
+	}
+
+	protected static Image loadImage(Path imagePath, int width, int height){
+		try (InputStream imageInputStream = Files.newInputStream(imagePath)){
+			return new Image(imageInputStream, width, height, false, false);
 		} catch (IOException e) {
+			log.error("Load entity image error", e);
 			throw new RuntimeException("Load entity image error", e);
 		}
 	}
 
-	public abstract void render(GameContextHolder context);
+	public abstract void render(GameInstanceContextHolder context);
 
-	public abstract void updateState(GameContextHolder context, double diff);
+	public abstract void updateState(GameInstanceContextHolder context, double diff);
 }
